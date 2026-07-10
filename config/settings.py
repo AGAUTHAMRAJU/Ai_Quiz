792ce60e529f064aac9e9fd892bbc44b94610626
AGAUTHAMRAJU <22q95a6703@gmail.com>
2026-07-11 00:12:26 +0530

    Ready for deployment

--- config/settings.py
+++ config/settings.py
@@ -55,10 +55,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = []
